22d2f6757d6fe94ba8122e82af06058a146931c2
Tweet Jefe <a.chavychalov@gmail.com>
2026-04-22 01:05:38 +0200

    fix: remove the problematic @Query method from ProjectRepository causing startup error

--- src/main/java/com/tasktrackergraphql/project/repository/ProjectRepository.java
+++ src/main/java/com/tasktrackergraphql/project/repository/ProjectRepository.java
@@ -13,13 +13,4 @@ import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<ProjectEntity, Long>, JpaSpecificationExecutor<ProjectEntity> {
     Window<ProjectEntity> findBy(ScrollPosition pos, Limit limit);
-
-    @Query("""
-        SELECT DISTINCT p FROM ProjectEntity p
-        LEFT JOIN p.assignees a
-        WHERE p.reporterId = :userId 
-        OR a = :userId 
-        OR EXISTS (SELECT t FROM TaskEntity t WHERE t.projectId = p.id AND t.assigneeId = :userId)
-    """)
-    Window<ProjectEntity> findAllProjectsByReporterId(Long userId, ScrollPosition position, Limit limit);
 }
